eee709a35dadf951894122b8b8839a3203bfd7b4
dexter <arturyongh2o@gmail.com>
2024-03-16 21:50:30 +0500

    #1 написал другой вариант

--- Повторение/repetition.py
+++ Повторение/repetition.py
@@ -1,9 +1,4 @@
 number = input()
-new_number = ''
-if len(number) == 5:
-    new_number = number[::-1]
-elif len(number) == 6:
-    new_number = number[0]
-    for i in range(1, len(number)):
-        new_number += number[-i]
-print(int(new_number))
+new_number = number[0: len(number)-5]
+new_number += number[len(number)-5:][::-1]
+print(new_number)
